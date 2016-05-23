bec3e44c440e0b67affafe8b22a0f7a8d312bb4c
haanj <joshua.haan@ymail.com>
2016-05-22 19:31:11 -0700

    style changes, adds project screenshots, fixes class switch

--- dev/js/app.js
+++ dev/js/app.js
@@ -17,47 +17,21 @@ var vm = new Vue({
       {
         name: "CF Package Manager",
         description: "RESTful package management server that can be deployed virtually anywhere. Corresponding Command Line Interface allows the user to upload directories to the server as packages, or to download existing packages. Features version control, user authentication/authorization, and working version control. Written in JavaScript using Node, Express, and MongoDB.",
-        imageUrl: "http://lorempixel.com/800/800/abstract",
+        imageUrl: "media/cfpm.png",
         githubUrl: "https://github.com/haanj/cfpm"
       },
       {
         name: "Story Blocks",
         description: "Story Blocks is a proof-of-concept for a new social-media platform. Utilizing new CSS features, Story Blocks displays images and articles on three dimensional spinning cubes, written in CSS. The app is a full MEAN-stack application, utilizing MongoDB, Express, Angular, and Node.",
-        imageUrl: "http://lorempixel.com/800/800/technics",
-        githubUrl: "https://github.com/haanj/cfpm"
+        imageUrl: "media/storyblocks.png",
+        githubUrl: "https://github.com/cube10/cube10"
       },
       {
         name: "Bitmap Transformer",
         description: "This is a command-line tool for manipulating non-palette .bmp images. It was written as an exercise to gain better understanding of computer bits. It is written entirely in JavaScript",
-        imageUrl: "http://lorempixel.com/800/800/business",
-        githubUrl: "https://github.com/haanj/bitmapManipulation"
-      },
-      {
-        name: "Story Blocks",
-        description: "Story Blocks is a proof-of-concept for a new social-media platform. Utilizing new CSS features, Story Blocks displays images and articles on three dimensional spinning cubes, written in CSS. The app is a full MEAN-stack application, utilizing MongoDB, Express, Angular, and Node.",
-        imageUrl: "http://lorempixel.com/800/800/technics",
-        githubUrl: "https://github.com/haanj/cfpm"
-      },
-      {
-        name: "Bitmap Transformer",
-        description: "This is a command-line tool for manipulating non-palette .bmp images. It was written as an exercise to gain better understanding of computer bits. It is written entirely in JavaScript",
-        imageUrl: "http://lorempixel.com/800/800/business",
-        githubUrl: "https://github.com/haanj/bitmapManipulation"
-      },
-      {
-        name: "Story Blocks",
-        description: "Story Blocks is a proof-of-concept for a new social-media platform. Utilizing new CSS features, Story Blocks displays images and articles on three dimensional spinning cubes, written in CSS. The app is a full MEAN-stack application, utilizing MongoDB, Express, Angular, and Node.",
-        imageUrl: "http://lorempixel.com/800/800/technics",
-        githubUrl: "https://github.com/haanj/cfpm"
-      },
-      {
-        name: "Bitmap Transformer",
-        description: "This is a command-line tool for manipulating non-palette .bmp images. It was written as an exercise to gain better understanding of computer bits. It is written entirely in JavaScript",
-        imageUrl: "http://lorempixel.com/800/800/business",
+        imageUrl: "media/bitmapManipulation.png",
         githubUrl: "https://github.com/haanj/bitmapManipulation"
       }
-
-
     ]
   }
 });
